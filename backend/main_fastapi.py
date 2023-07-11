1b9c1b668e5fa5d9d1209c43b24a397a346841b2
tomo-cps <tomoaki.ohkawa@cps.akita-pu.ac.jp>
2023-07-12 01:49:28 +0900

    サイバーエージェントのLLMを使って通信できた！！

--- backend/main_fastapi.py
+++ backend/main_fastapi.py
@@ -1,50 +1,29 @@
+## uvicorn main_fastapi:app --host 0.0.0.0 --port 8085
 import os
 from fastapi import FastAPI
 # from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from langchain.document_loaders import PyPDFLoader
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.llms import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# Embedding用
-os.environ["OPENAI_API_TYPE"] = "azure"
-os.environ["OPENAI_API_KEY"] = "YOUR_OPENAI_API_KEY"
-os.environ["OPENAI_API_BASE"] = "https://***.openai.azure.com/"
-
-# テキストローダーを定義
-loader = PyPDFLoader("./data/twitter.pdf")
-
-# ベクトル化用モデルを定義
-embeddings = OpenAIEmbeddings(
-    model='text-embedding-ada-002',
-    deployment="teama-embedding-ada-002",
-    chunk_size=1)
-
-# ChatGPT用
-os.environ["OPENAI_API_KEY"] = "YOUR_OPENAI_API_KEY"
-os.environ["OPENAI_API_BASE"] = "https://***.openai.azure.com/"
-llm = OpenAI(model_name='gpt-35-turbo', model_kwargs={"deployment_id":"teama-gpt-35-turbo"})
-
-# テキストをベクトル化/インデックス化
-index = VectorstoreIndexCreator(embedding=embeddings).from_loaders([loader])
-print(index)
+model = AutoModelForCausalLM.from_pretrained("cyberagent/open-calm-small", device_map="auto", torch_dtype=torch.float16)
+tokenizer = AutoTokenizer.from_pretrained("cyberagent/open-calm-small")
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    # allow_origins=[
-    #     "http://54.238.157.42/",
-    #     "http://54.238.157.42/chat1",
-    #     "http://54.238.157.42/chat2",
-    #     "http://54.238.157.42/chat3",
-    #     "http://54.238.157.42/chat4",
-    #     "http://54.238.157.42/chat5",
-    #     "http://54.238.157.42/chat6",
-    # ],
+    # allow_origins=["*"],
+    allow_origins=[
+        "https://api.tomoaki-ohkawa.com",
+        "https://api.tomoaki-ohkawa.com/chat1",
+        "https://api.tomoaki-ohkawa.com/chat2",
+        "https://api.tomoaki-ohkawa.com/chat3",
+        "https://api.tomoaki-ohkawa.com/chat4",
+        "https://api.tomoaki-ohkawa.com/chat5",
+        "https://api.tomoaki-ohkawa.com/chat6",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -60,8 +39,22 @@ def read_root():
 
 @app.post("/ask")
 def ask_question(question: Question):
-    response = index.query(llm=llm, question=question.question)
-    res = llm(response+"/n"+"/n"+"上のテキスト情報の出力がキリが悪いので，キリをよくしてください.なお，高校生でもわかりやすい出力をしてください")
+    inputs = tokenizer(str(question), return_tensors="pt").to(model.device)
+    with torch.no_grad():
+        tokens = model.generate(
+            **inputs,
+            max_new_tokens=64,
+            do_sample=True,
+            temperature=0.7,
+            top_p=0.9,
+            repetition_penalty=1.05,
+            pad_token_id=tokenizer.pad_token_id,
+        )
+        
+    output = tokenizer.decode(tokens[0], skip_special_tokens=True)
+    # response = index.query(llm=llm, question=question.question)
+    # res = llm(response+"/n"+"/n"+"上のテキスト情報の出力がキリが悪いので，キリをよくしてください.なお，高校生でもわかりやすい出力をしてください")
     # max_length = 200  # 任意の最大文字数を設定
     # shortened_response = response[:max_length]
-    return {"answer": res}
+    return {"answer": output}
+    # return {"answer": res}
