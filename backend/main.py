d869871743b158409993359d16ec7087e43ed191
tomo-cps <tomoaki.ohkawa@cps.akita-pu.ac.jp>
2023-07-15 18:58:05 +0900

    [backend] データベースと連携させ，ユーザの質問に応じて動的にWordCloudが変化するようにしました

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,96 @@
+## uvicorn main:app --host 0.0.0.0 --port 8000
+## uvicorn main_fastapi:app --host 0.0.0.0 --port 8085
 import os
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from langchain.document_loaders import PyPDFLoader
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.llms import OpenAI
-from langchain.embeddings import OpenAIEmbeddings
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from fastapi.responses import FileResponse
+import sqlite3
+import re
+import base64
+from wordclouds import wc
 
-# Embedding用
-def mains():
-    os.environ["OPENAI_API_TYPE"] = "azure"
-    os.environ["OPENAI_API_KEY"] = "YOUR_OPENAI_API_KEY"
-    os.environ["OPENAI_API_BASE"] = "https://***.openai.azure.com/"
+model = AutoModelForCausalLM.from_pretrained("cyberagent/open-calm-small", device_map="auto", torch_dtype=torch.float16)
+tokenizer = AutoTokenizer.from_pretrained("cyberagent/open-calm-small")
 
-    # テキストローダーを定義
-    loader = PyPDFLoader("./data/twitter.pdf")
+# model = AutoModelForCausalLM.from_pretrained("cyberagent/open-calm-7b", device_map="auto", torch_dtype=torch.float16)
+# tokenizer = AutoTokenizer.from_pretrained("cyberagent/open-calm-7b")
 
-    # ベクトル化用モデルを定義
-    embeddings = OpenAIEmbeddings(
-        model='text-embedding-ada-002',
-        deployment="teama-embedding-ada-002",
-        chunk_size=1)
+def get_ans(question):
+    inputs = tokenizer(str(question), return_tensors="pt").to(model.device)
+    with torch.no_grad():
+        tokens = model.generate(
+            **inputs,
+            max_new_tokens=64,
+            do_sample=True,
+            temperature=0.7,
+            top_p=0.9,
+            repetition_penalty=1.05,
+            pad_token_id=tokenizer.pad_token_id,
+        )
+        
+    ans = tokenizer.decode(tokens[0], skip_special_tokens=True)
+    return ans
+app = FastAPI()
 
-    # ChatGPT用
-    os.environ["OPENAI_API_KEY"] = "YOUR_OPENAI_API_KEY"
-    os.environ["OPENAI_API_BASE"] = "https://***.openai.azure.com/"
-    llm = OpenAI(model_name='gpt-35-turbo', model_kwargs={"deployment_id":"teama-gpt-35-turbo"})
-    # テキストをベクトル化/インデックス化
-    index = VectorstoreIndexCreator(embedding=embeddings).from_loaders([loader])
-    return index
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://gmc.cps.akita-pu.ac.jp/",
+        "https://gmc.cps.akita-pu.ac.jp/chat1",
+        "https://gmc.cps.akita-pu.ac.jp/chat2",
+        "https://gmc.cps.akita-pu.ac.jp/chat3",
+        "https://gmc.cps.akita-pu.ac.jp/chat4",
+        "https://gmc.cps.akita-pu.ac.jp/chat5",
+        "https://gmc.cps.akita-pu.ac.jp/chat6",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
 
-print(mains())
+class Question(BaseModel):
+    name: str
+    path: str
+    question: str
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/ask")
+def ask_question(question: Question):
+    name = question.name
+    path = question.path
+    question = question.question
+    output = wc.CreateWordCloud(path, name)
+    wc_path = output.main()
+    print(wc_path)
+
+    ans = get_ans(question)
+    ans = re.sub(r'\);.*$', '', ans)
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    data = {
+        "path": path,
+        "user": name,
+        "question": question,
+        "answer": ans,
+        "wc_path": wc_path
+        }
+
+    c.execute('''
+        INSERT INTO user_table (path, user, question, answer, wc_path)
+        VALUES (:path, :user, :question, :answer, :wc_path)
+    ''', data)
+
+    conn.commit()
+    conn.close()
+    with open(wc_path, "rb") as f:
+        image_data = f.read()
+        encoded_image = base64.b64encode(image_data).decode("utf-8")
+    return {"answer": ans, "wordcloud": encoded_image}
--- backend/startup.sh
+++
@@ -1,2 +0,0 @@
-pip install -r requirements.txt
-python -m uvicorn --host 0.0.0.0 my_fastapi:app
